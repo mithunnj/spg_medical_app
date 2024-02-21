029b146e56ec21fa8e919d98deef41f0e5c75183
Mithun Jothiravi <mithun.jothiravi@mail.utoronto.ca>
2024-02-20 19:25:29 -0800

    Amplify and Auth setup

---
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "frontend8f5d729b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
