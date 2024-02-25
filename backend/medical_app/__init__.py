4cfc3ccf6e8c4384b671ea8ed36078274378b3b0
Mithun Jothiravi <mithun.jothiravi@mail.utoronto.ca>
2024-02-25 11:19:00 -0800

    It is time to start fresh

--- backend/medical_app/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'medical_app.apps.MedicalAppConfig'
--- backend/medical_app/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class MedicalAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'medical_app'
-
-    def ready(self):
-        import medical_app.signals
